bb33b333b01558a95c1c804be2ee85cebb573743
MarinaAnna <marver2525@gmail.com>
2022-11-27 14:38:09 +0700

    first project

--- ca4/ca4/Program.cs
+++ ca4/ca4/Program.cs
@@ -7,6 +7,42 @@
             Console.WriteLine("Hello World!");
             Console.WriteLine("Oh, hi, Mark!");
             Console.ReadKey();
+
+            int A, B, C;
+
+            Console.WriteLine("Введите два числа : ");
+            A = Convert.ToInt32(Console.ReadLine());
+            B = Convert.ToInt32(Console.ReadLine());
+            C = A + B;
+            Console.WriteLine("Сумма данных чисел:");
+            Console.WriteLine(C);
+            Console.ReadLine();
+            if (A > B)
+            {
+                C = A - B;
+            }
+            else
+            {
+                C = B - A;
+            }
+            Console.WriteLine("Разность данных чисел:");
+            Console.WriteLine(C);
+            Console.ReadLine();
+            C = A * B;
+            Console.WriteLine("Произведение данных чисел:");
+            Console.WriteLine(C);
+            Console.ReadLine();
+            if (A > B)
+            {
+                C = A / B;
+            }
+            else
+            {
+                C = B / A;
+            }
+            Console.WriteLine("Деление данных чисел:");
+            Console.WriteLine(C);
+            Console.ReadLine();
         }
     }
 
