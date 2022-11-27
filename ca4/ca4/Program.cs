d45458806b41eb83528d0e2a172c506ecc593a48
MarinaAnna <marver2525@gmail.com>
2022-11-27 14:03:28 +0700

    first project

---
+++ ca4/ca4/Program.cs
@@ -0,0 +1,13 @@
+﻿namespace ca4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            Console.WriteLine("Oh, hi, Mark!");
+            Console.ReadKey();
+        }
+    }
+
+}
